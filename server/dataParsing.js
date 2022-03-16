19f89e7c768ae9165c863e457d435449d126db25
Salt Developer <salt-dev@DEV132.local>
2022-03-16 19:03:33 +0100

    boilerplate ready

---
+++ server/dataParsing.js
@@ -0,0 +1,39 @@
+const recipesParser = recipe => {
+  const recipeId = recipe.recipe.uri.split('_')[1];
+  const recipeName = recipe.recipe.label;
+  const recipeImg = recipe.recipe.images.REGULAR.url;
+  const recipeSource = recipe.recipe.source;
+  const recipeUrl = recipe.recipe.url;
+  const recipeIngredientsArray = recipe.recipe.ingredientLines;
+  const recipeCalories = recipe.recipe.calories.toString().split('.')[0];
+  const recipeCuisineType = recipe.recipe.cuisineType[0];
+  const recipeMealType = recipe.recipe.mealType[0];
+  const recipedishType = recipe.recipe.dishType[0];
+  const recipeDataObj = {
+    recipeId,
+    recipeName,
+    recipeImg,
+    recipeSource,
+    recipeUrl,
+    recipeIngredientsArray,
+    recipeCalories,
+    recipeCuisineType,
+    recipeMealType,
+    recipedishType
+  };
+
+  return recipeDataObj;
+}
+
+const dataToArray = data => {
+  const recipesArray = data.hits;
+  const newArrayData = [];
+  recipesArray.forEach(recipe => {
+  const parsedRecipe = recipesParser(recipe);
+    newArrayData.push(parsedRecipe);
+  });
+  return newArrayData;
+};
+
+module.exports.recipesParser = recipesParser;
+module.exports.dataToArray = dataToArray;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require('express');
+const dataParsing = require('./dataParsing')
+const fetch = require('node-fetch');
+const cors = require('cors')
+require('dotenv').config();
 
 const app = express()
 
 const port = 3030
 
+app.use(cors())
+
 app.listen(port, ()=> console.log(`Hey I'm here listening on ${port}!`))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/api/recipes', async (req, res) => {
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${'lime'}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&ingr=1-5&time=15-30`
+  const response = await fetch(url);
+  const data = await response.json(response);
+  const parsedData = dataParsing.dataToArray(data);
+
+  res.json(parsedData);
+});
