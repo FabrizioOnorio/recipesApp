3c42ff3859ee44f421b9e88dbf9c30a0f7f02edc
Salt Developer <salt-dev@DEV132.local>
2022-03-18 10:00:14 +0100

    added hover effect to form

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -11,7 +11,7 @@ const About = () => {
         <Link to="/about">About</Link>
       </nav>
       <section className='about__header'>
-        <h2>Google Recipes for Hackday in &lt;/salt&gt;</h2>
+        <h2>Google Recipes for Hackday in &lt;/salt&gt;</h2><br></br>
         <p>Google Recipes is a full stack application built for a hackday,<br></br>
          it retrieves datas from Edamam api and it let's you find recipes<br></br>
           that take between 15 and 30 minutes to make based on the ingredients<br></br>
